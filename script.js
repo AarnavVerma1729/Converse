04033ea517d7ce675a8254f07eebf7734a980627
AarnavVerma1729 <verma.aarnav21@gmail.com>
2025-02-06 07:51:49 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("H").onmouseover = function() {
+  document.getElementById('shoe').src = "https://www.converse.com/on/demandware.static/-/Library-Sites-ConverseEU01SharedLibrary/default/dwcaf599de/firstspirit/converse-uk/media/homepage_1/2025_spring/D-Converse-Homepage-P2A-High-Tops.jpg";
+};
+document.getElementById("L").onmouseover = function() {
+  document.getElementById('shoe').src = "https://www.converse.com/on/demandware.static/-/Library-Sites-ConverseEU01SharedLibrary/default/dw6bc56c5b/firstspirit/converse-uk/media/homepage_1/2025_spring/D-Converse-Homepage-P2B-Low-Tops.jpg";
+};
+document.getElementById("P").onmouseout = function() {
+  document.getElementById('shoe').src = "https://www.converse.com/on/demandware.static/-/Library-Sites-ConverseEU01SharedLibrary/default/dwd7cbc3ea/firstspirit/converse-uk/media/homepage_1/2025_spring/D-Converse-Homepage-P2C-Platforms.jpg";
+};
+
+document.getElementById("C").onmouseout = function() {
+  document.getElementById('shoe').src = "https://www.converse.com/on/demandware.static/-/Library-Sites-ConverseEU01SharedLibrary/default/dwcb592d9f/firstspirit/converse-uk/media/homepage_1/2025_spring/D-Converse-Homepage-P2D-Custom.gif";
+};
